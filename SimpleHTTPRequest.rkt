b5ff0491f3ff14271268d11c3dd6d55ad96ca29a
GER13117 <okke.warner@gmail.com>
2021-11-02 22:19:49 +0100

    Create SimpleHTTPRequest.rkt

---
+++ SimpleHTTPRequest.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require json)
+(require net/url)
+(require http/request)
+
+(bytes->jsexpr (call/input-request
+  "1.1"
+  "GET"
+  "http://192.168.180.34/api/"
+  empty
+  read-entity/bytes))
+
+(bytes->jsexpr (call/output-request
+  "1.1"
+  "PUT"
+  "http://192.168.180.34/api/api-search.php"
+  (jsexpr->bytes (hasheq 'search "one"))
+  #f
+  empty
+  read-entity/bytes))
