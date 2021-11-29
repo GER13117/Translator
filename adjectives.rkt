a0e816a1f934b24693a371c379c300aefb4f039c
jowali <94540087+jowali@users.noreply.github.com>
2021-11-29 23:00:51 +0100

    translator teils formatiert und adjectives mit starken Plural dativ angefangen

--- adjectives.rkt
+++ adjectives.rkt
@@ -18,21 +18,51 @@
 ;Genitiv: the ball of the small boy -> der Ball des kleinEN Jungen
 
 (define (AdjectiveQuery adjective_eng)
-  (query-value mdbc (string-append "SELECT ger_adj FROM adjectives WHERE eng_adj=" "'" adjective_eng "'")))
-   ;funktioniert wie regVerbQuery, muss ggf. auf die Tabellen angepasst werden
+  (query-value mdbc (string-append "SELECT ger_adj FROM adjectives WHERE eng_adj=" "'" adjective_eng "'"))
+  )
+;funktioniert wie regVerbQuery, muss ggf. auf die Tabellen angepasst werden
 
 (define (getAdjective adjective pos wordTypeList input)
   (cond
-    [(eq? (wordsbefore pos input) #t) (string-append (AdjectiveQuery adjective) "en")]
-    [else "kein genitiv"])
+    [(ofBefore pos input) (string-append (AdjectiveQuery adjective) "en")] ;wenn bis zu drei Wörter davor ein "of" steht -> Genitiv und "en" wird drangehängt
+    [(eq? (getCase adjective) "dativ")
+     (cond
+       [(weakDeclination pos input) (string-append (AdjectiveQuery adjective) "en")] ;bei schwacher Deklination
+       [else                                                                         ;bei starker Deklination
+        (cond
+          [(eq? (nextNoun pos input) plural) (string-append (AdjectiveQuery adjective) "en")] ;bei Plural
+          )
+        ]
+       )
+     ]
+    )
   )
-;wenn bis zu drei Wörter davor ein "of" steht -> Genitiv und "en" wird drangehängt
+;nextNoun -> holt sich das nächste Nomen
+        
+       
 
-(define (wordsbefore pos input)
+
+
+
+(define (weakDeclination pos input)
+  (not
+   (eq?
+    (or
+     (member (list-ref input (- pos 1)) '("der" "die" "das"))
+     (isPronoun (list-ref input (- pos 1)))
+     )
+    #f
+    )
+   )
+  )
+
+;schaut, ob schwach dekliniert werden muss
+
+(define (ofBefore pos input)
   (cond
-    [eq? (list-ref input (- pos 1)) "of"]
-    [eq? (list-ref input (- pos 2)) "of"]
-    [eq? (list-ref input (- pos 3)) "of"]
+    [(eq? (list-ref input (- pos 1)) "of")]
+    [(eq? (list-ref input (- pos 2)) "of")]
+    [(eq? (list-ref input (- pos 3)) "of")]
     [else #f]))
 ;schaut, ob eins der drei Wörter vor der pos "of" ist
 
